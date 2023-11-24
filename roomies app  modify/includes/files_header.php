4d78a2e494d2bc5ae437cd8e5b2cc47b1e12f6a7
chitradip97 <123654669+chitradip97@users.noreply.github.com>
2023-11-24 20:49:23 +0530

    Project added

---
+++ roomies app  modify/includes/files_header.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>roomies</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    <!-- <link rel="stylesheet" type="text/css" href="./assets/js/script.css"> -->
+</head>
+<body>
+   <!-- Top-Navbar -->
+<div class="top_nav" style="margin-bottom:10px;" >
+    <nav class="     navbar-light bg-info">
+        <div class="container-fluid">
+            <div class="row  " style="min-height:56px;">
+                <div class="col-md-4 col-sm-6 mb-2" style="line-height: 56px;">
+                    <div class="collapse navbar-collapse d-flex flex-row " >
+                        <div>
+                        <img src="../assets/img/app_logo.png" alt="" style="width:50px; height:50px margin-top:100px">
+                    </div>
+                    <div>
+                    <h3 class="ps-2 mt-2"><b>Expence solution</b></h3> 
+                    </div>
+                </div>
+            </div>
+                <div class="col-md-8 col-sm-6 mt-1 float-md-end">
+                <div class="collapse navbar-collapse d-flex flex-row-reverse " >
+                <li ><a href="" class="icon"><i class="fa fa-user icon_style ms-4" aria-hidden="true"></i></a></li>
+                <li ><a href="" class="icon"><i class="fa fa-bell icon_style ms-4" aria-hidden="true"></i></a></li>
+                <li ><a href="" class="icon"><i class="fa fa-comment icon_style ms-4" aria-hidden="true"></i></a></li>
+                <li ><a href="" class="icon"><i class="fa fa-shopping-cart icon_style ms-4" aria-hidden="true"></i></a></li>
+        </div>
+        </div>
+           
+    </nav>
+</div>
+
+<!-- sidebar -->
+<!-- left sidebar -->
+<div class="body_area clearfix " >
+    <div class="left_sidebar mt-2 ms-2">
+        <nav id="sidebar" class="bg_color  p-4  ">
+            <a href="dashboard.php" class="anchor "><h5 class="bar_option">News Feed</h5></a>
+            <a href="create_order.php" class="anchor"><h5 class="bar_option">My Expense</h5></a>
+            <a href="orders_list.php" class="anchor"><h5 class="bar_option">Others Expense</h5></a>
+            <a href="create_order.php" class="anchor"><h5 class="bar_option">Contribution</h5></a>
+            <a href="orders_list.php" class="anchor"><h5 class="bar_option">Others</h5></a>
+        </nav>
+    </div>
+
+    <!-- body content -->
+
+
+  
+    <!-- right sidebar    -->
+
+    <div class="right_sidebar mt-2 ms-2">
+        <nav id="sidebar" class="bg_color  p-4  ">
+            <a href="dashboard.php" class="anchor "><h5 class="bar_option">News Feed1</h5></a>
+            <a href="create_order.php" class="anchor"><h5 class="bar_option">My Expense</h5></a>
+            <a href="orders_list.php" class="anchor"><h5 class="bar_option">Others Expense</h5></a>
+            <a href="create_order.php" class="anchor"><h5 class="bar_option">Contribution</h5></a>
+            <a href="orders_list.php" class="anchor"><h5 class="bar_option">Others</h5></a>
+        </nav>
+    </div>
+
+</div>        
+           
+                    
+                
+            
+            
+            
+            
+            
+           
+       
+    </nav>
+</div>
+
+
+        
+</body>
+</html>
