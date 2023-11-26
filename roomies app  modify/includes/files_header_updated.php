44ab4320233fa534ca76498823f40670502583b9
chitradip97 <chitradip97@gmail.com>
2023-11-26 15:43:12 +0530

    modify

--- roomies app  modify/includes/files_header_updated.php
+++ roomies app  modify/includes/files_header_updated.php
@@ -63,39 +63,45 @@
             <!-- <div class="container-fluid "> -->
                 
                 <div style="background-color:DodgerBlue;text-align: center;"><h4 style="display:inline-block; ">Chat Messages</h4></div>
-                <div class="chat mt-3">
+                <div class="chat mt-2">
                     <div class="container-fluid lighter" >
-                        <img src="" alt="Avatar" style="width:100%;">
-                        <p class="chat_font">Hello. How are you today?</p>
+                        <img src="" alt="Avatar" style="width:100%; ">
                         <span class="time-right">11:00</span>
+                        <p class="chat_font">Hello. How are you today?</p>
+                        
                     </div>
 
                     <div class="container-fluid darker">
                         <img src="" alt="Avatar" class="right" style="width:100%;">
-                        <p class="chat_font">Hey! I'm fine. Thanks for asking!</p>
                         <span class="time-left">11:01</span>
+                        <p class="chat_font">Hey! I'm fine. Thanks for asking!</p>
+                        
                     </div>
 
                     <div class="container-fluid lighter">
                         <img src="" alt="Avatar" style="width:100%;">
-                        <p class="chat_font">Sweet! So, what do you wanna do today?</p>
                         <span class="time-right">11:02</span>
+                        <p class="chat_font">Sweet! So, what do you wanna do today?</p>
+                        
                     </div>
 
                     <div class="container-fluid darker">
                         <img src="" alt="Avatar" class="right" style="width:100%;">
-                        <p class="chat_font">Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
                         <span class="time-left">11:05</span>
+                        <p class="chat_font">Nah, I dunno. Play soccer.. or learn more coding</p>
+                        
                     </div>
                     <div class="container-fluid lighter">
                         <img src="" alt="Avatar" style="width:100%;">
-                        <p class="chat_font">Sweet! So, what do you wanna do today?</p>
                         <span class="time-right">11:06</span>
+                        <p class="chat_font">Sweet! So, what do you wanna do today?</p>
+                        
                     </div>
                     <div class="container-fluid darker">
-                        <img src="" alt="Avatar" style="width:100%;">
-                        <p class="chat_font">Sweet! So, what do you wanna do today?</p>
+                        <img src="" alt="Avatar" class="right" style="width:100%;">
                         <span class="time-left">11:07</span>
+                        <p class="chat_font">Sweet! So, what do you wanna do today?</p>
+                        
                     </div>
                 </div>
                 <textarea class="form-control mt-1" rows="2" col="3" id="comment"></textarea>
